357e243a65abce7668dfad67c1d10ab802281035
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-09-07 07:48:12 +0300

    test safari

--- text.js
+++ text.js
@@ -7,10 +7,30 @@ const voiceSelectEle = document.querySelector("select");
 
 
 
+function loadVoices() {
+  let voices = speechSynthesis.getVoices();
+
+  voiceSelectEle.innerHTML = "";
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.text = `(${voice.name} ${voice.lang})`;
+
+    option.selected = voice.name === "Google US English";
+    voiceSelectEle.appendChild(option);
+  });
+}
+loadVoices()
+
+window.speechSynthesis.onvoiceschanged = () => loadVoices();
+
+
+
+
 
 // Variables
 let speech = new SpeechSynthesisUtterance();
-let voices = [];
 
 
 
@@ -29,29 +49,10 @@ function speakTextToVoice() {
 
 
 
-function loadVoices() {
-  voices = window.speechSynthesis.getVoices();
 
-  voiceSelectEle.innerHTML = "";
-
-  voices.forEach((voice) => {
-    const option = document.createElement("option");
-    option.value = voice.name;
-    option.text = `(${voice.name} ${voice.lang})`;
-
-    option.selected = voice.name === "Google US English";
-    voiceSelectEle.appendChild(option);
-  });
-}
 
 
 
 
 // Events
-window.speechSynthesis.onvoiceschanged = () => loadVoices();
-
-voiceSelectEle.addEventListener("change", () => {
-  speech.voice = voices[voiceSelectEle.value];
-});
-
 listenButton.addEventListener("click", () => speakTextToVoice());
