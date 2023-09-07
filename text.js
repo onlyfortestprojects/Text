ec7438e3fc21b6f5c8c716be1c723137887717d6
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-09-07 07:31:38 +0300

    test

--- text.js
+++ text.js
@@ -1,35 +1,54 @@
 "use strict";
 
 // Selectors
-const textAreaEle = document.querySelector("textarea")
-const listenButton = document.querySelector("#listen")
+const textAreaEle = document.querySelector("textarea");
+const listenButton = document.querySelector("#listen");
 const voiceSelectEle = document.querySelector("select");
+
+
+
 
 // Variables
 let speech = new SpeechSynthesisUtterance();
 let voices = [];
 
+
+
+
 // Functions
 function speakTextToVoice() {
   speech.text = textAreaEle.value;
-  window.speechSynthesis.speak(speech);
+
+  if (voiceSelectEle.value)
+    speech.voice = speechSynthesis
+      .getVoices()
+      .filter((voice) => voice.name == voiceSelectEle.value)[0];
+
+  speechSynthesis.speak(speech);
 }
 
-function getVoicesToSelect() {
-  // Clear existing options
-  voiceSelectEle.innerHTML = '';
 
+
+function loadVoices() {
   voices = window.speechSynthesis.getVoices();
 
-  voices.forEach((voice, i) => {
-    // const option = new Option(voice.name, voice.name); // Use voice name as both text and value
-    // voiceSelectEle.add(option);
-		document.body.innerHTML += `<div>${voice.name}</div>`
+  voiceSelectEle.innerHTML = "";
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.text = `(${voice.name} ${voice.lang})`;
+
+    option.selected = voice.name === "Google US English";
+    voiceSelectEle.appendChild(option);
   });
 }
 
+
+
+
 // Events
-window.speechSynthesis.onvoiceschanged = () => getVoicesToSelect();
+window.speechSynthesis.onvoiceschanged = () => loadVoices();
 
 voiceSelectEle.addEventListener("change", () => {
   speech.voice = voices[voiceSelectEle.value];
